17815b0b70740a4996d1bdb9cc3a6c24225f1f03
JayDub21 <Justin.Wofford.21@gmail.com>
2021-02-11 12:59:56 -0600

    + <Users />

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 // import Movie from './hoc/Movie';
-import Counter from './hooks/Counter';
+// import Counter from './hooks/Counter';
+import Users from './hooks/Users';
 
 function App() {
   return (
-    <Counter />
+    <Users />
+
+    // <Counter />
 
     //   <Movie id={1} />;
   );
